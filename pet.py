4d333c6700667c4f3e13232bca27b078b93a8cf2
Kesa1203 <kesasylvesterlee@gmail.com>
2025-04-17 22:31:23 +0200

    Initial project setup with pet.py and main.py

---
+++ pet.py
@@ -0,0 +1,27 @@
+class Pet:
+    # Constructor to initialize the pet's attributes
+    def __init__(self, name):
+        self.name = name  # Name of the pet
+        self.hunger = 15  # Hunger level (higher means hungrier)
+        self.energy = 10  # Energy level
+        self.happiness = 20  # Happiness level
+        self.tricks = []  # List of tricks the pet knows
+
+    def eat(self):
+        pass
+
+    def sleep(self):
+        pass
+
+    def play(self):
+        pass
+
+    def train(self, trick):
+        pass
+
+    def show_tricks(self):
+        pass
+
+    def get_status(self):
+        pass
+
