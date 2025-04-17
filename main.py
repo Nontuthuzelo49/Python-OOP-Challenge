a031b2cdf2c4d0167ccb9e078abfed0babd4ff71
RencyBoreh <jeptanuirency313@gmail.com>
2025-04-17 20:34:25 +0300

    my-oop-challenge

---
+++ main.py
@@ -0,0 +1,12 @@
+from Pet import Pet  # Importing the Pet class from pet.py
+
+# Creating an instance of the Pet class
+my_pet = Pet("Bobby")
+
+# Calling methods to interact with your pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("Rolling Over")
+my_pet.show_tricks()
