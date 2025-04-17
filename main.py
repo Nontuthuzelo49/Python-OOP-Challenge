d48345927bcab2ffb8546f6a58bf940475038629
gatuati <125788606+gatuati@users.noreply.github.com>
2025-04-17 21:10:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from pet import Pet 
+my_pet = Pet("Fluffy")
+my_pet.get_status()   
+my_pet.eat()           
+my_pet.sleep()         
+my_pet.play()          
+my_pet.get_status()    
+my_pet.train("roll over") 
+my_pet.train("fetch")     
+my_pet.show_tricks()    
