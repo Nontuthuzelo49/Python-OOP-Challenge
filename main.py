c18b70648131835519ced2215e943e748574832f
Christabel-Akpene <dc.akpene@gmail.com>
2025-04-18 11:24:31 +0000

    Submit oop challenge

---
+++ main.py
@@ -0,0 +1,18 @@
+from pet import Pet
+
+kitty = Pet("Max")
+kitty.play()
+kitty.train("Pet a donkey")
+kitty.train("Fetch a stick")
+# print(kitty.get_status())
+kitty.sleep()
+kitty.eat()
+kitty.play()
+kitty.play()
+kitty.sleep()
+kitty.eat()
+kitty.get_status()
+
+
+puppy = Pet("meow")
+puppy.get_status()
